25961c9086e5a7fd8bbe5aea4ee60d57d6eb2817
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-01-04 20:28:26 +0100

    Finish task 10.2

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	console.log('DOM loaded')
+})
+
+var span = $('span').each(function(index, element) {
+	if (index % 2 == 0) {
+		$(element).css('color', 'red');
+	};
+});
+
+var para = $('p').each(function(index, element) {
+	$(element).append('<button data-tmp="'+ index +'" type="button">Click me!</button>');
+});
+
+$('button').click(function() {
+	alert($(this).attr('data-tmp'));
+});
