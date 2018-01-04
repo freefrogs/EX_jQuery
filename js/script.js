848fd0171934249b0969de0f26c5f75017aac7b3
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-01-04 20:39:52 +0100

    Add last correction

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function(){
 var span = $('span').each(function(index, element) {
 	if (index % 2 == 0) {
 		$(element).css('color', 'red');
-	};
+	}
 });
 
 var para = $('p').each(function(index, element) {
